9f857615fbc053f4c6317cbe37864dbe4c4ac6fe
blablabla891015 <83815454+blablabla891015@users.noreply.github.com>
2023-02-18 00:05:27 +0800

    finish

--- Closest Prime Numbers in Range/main.cpp
+++ Closest Prime Numbers in Range/main.cpp
@@ -1,51 +1,53 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 using namespace std;
-bool is_prime(int number,vector<int> prime_list){
-    for (int i=0;i<prime_list.size();i++){
-        if(number%prime_list[i]==0){
-            return false;
+vector<bool> table(1000000+1, true);
+vector<int> closestPrimes(int left, int right) {
+    int now=-1;
+    int next=-1;
+    int answer_r=-1;
+    int answer_l=-1;
+    int min=1000000000;
+    if(table[1]){
+        table[1]=false;
+        for(int i=2;i<1000000+1;i++){
+            for(long long j=(long long)i*i;j<1000000+1;j=j+i){
+                table[j]=false;
+            }
         }
     }
-    for (int i=2;i<number;i++){
-            if(number%i==0){
-                return false;
-            }
-    }
-    return true;
-}
-vector<int> closestPrimes(int left, int right) {
-    vector<int> prime_list;
-    int answer_left=-1;
-    int answer_right=-1;
-    int min=1000000000;
     if(left==1){
         left=left+1;
     }
-    for(int number=left;number<=right;number++){
-        if(is_prime(number,prime_list)){
-            prime_list.push_back(number);
-            if(prime_list.size()>=2){
-                if((prime_list[prime_list.size()-1]-prime_list[prime_list.size()-2])<min){
-                    answer_left=prime_list[prime_list.size()-2];
-                    // cout<<answer_left;
-                    answer_right=prime_list[prime_list.size()-1];
-                    min=prime_list[prime_list.size()-1]-prime_list[prime_list.size()-2];
-                }
-
-
+    for(int number=left;number<right+1;number++){
+        cout<<now<<" "<<next<<" "<<min<<" ";
+        if(table[number]){
+            now=next;
+            next=number;
+            if(now != -1 && next != -1 && next-now<min){
+                min=next-now;
+                answer_l=now;
+                answer_r=next;
             }
+            
+
         }
     }
-    cout<<answer_left<<" "<<answer_right;
-    return {answer_left,answer_right};
+    // cout<<now<<" "<<next;
+    if(answer_l != -1 && answer_r != -1){
+        return {answer_l,answer_r};
+    }
+    else{
+        return {-1,-1};
+    }
     
     
 
         
     }
 int main(void){
-    closestPrimes(88,100);
+    closestPrimes(10,19);
     return 0;
     // while(true){
 
